7eaaf2dbf5587ee052389d489317387f9e15c862
Rayan <rayanchambet1@gmail.com>
2022-10-18 21:03:47 +0200

    Portfolio 🎯 About ✅ Home ✅

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -5,56 +5,33 @@ const About = () => {
     return(
         <section className="container__about">
             <h2>About</h2>
-            <div className="container__about-top">
-                <div className="container__about-card">
-                    <img src="/assets/Rayan_Crimages.jpg" alt="Rayan Chambet" />
+            <div className="container__about-card">
+                <img src="/assets/Rayan_Crimages.jpg" alt="Rayan Chambet" className="profilePic"/>
+                <div className="description">
                     <h3>Hi, I'm Rayan. 👋</h3>
                     <p>I'm a 25 years old French dude. <br/>
                         I’m observant and curious, with a strong sense of competitivity and <strong>loyalty.</strong><br/>
                         <strong>People</strong> and <strong>environment</strong> are at the heart of my concerns, therefore building <strong>responsible web solutions</strong> is a key value in my work.
-                        </p>
-                </div>
-                <div className="container__about-bottom">
-                    <div className="container__about-textLeft">
-                        <p>
-                            After a first a degree of Web Developement, I obtained a Bachelor Degree to be a <strong>Software Developer</strong> specialized in <strong>Frontend</strong>.
-                        </p>
-                    </div>
-                    <div className="container__about-textRight">
-                        <p>
-                            Today, I run <strong>BLOB Agency</strong>, a <strong>Webdesign</strong> company with incredible talents such as <a href="https://www.linkedin.com/in/alexiane-grieu-864084152/"><em>Alexiane Grieu</em></a>, UI/UX Designer.
-                        </p>
-                    </div>
-                    <div className="container__about-textLeft">
-                        <p>
-                            Wether you want to bring build your <strong>project,</strong>start a <strong>business</strong> or talk about a <strong>job opportunity</strong>, feel free to reach out!
-                        </p>
-                    </div>
+                    </p>
                 </div>
             </div>
-           
-            {/* <div className="container__about-textLeft">
+            <div className="container__about-textLeft">
                 <p>
-                    After a first a degree of Web Developement, I obtained a Bachelor Degree to be a <strong>Software Developer</strong> specialized in <strong>Frontend</strong>.
+                After a first a degree of Web Developement, I obtained a Bachelor Degree to be a <strong>Software Developer</strong> specialized in <strong>Frontend</strong>.
                 </p>
-            </div> */}
-            {/* <div className="container__about-textRight">
-            <p>I'm a 25 years old French dude. <br/>
-                I’m observant and curious, with a strong sense of competitivity and <strong>loyalty.</strong><br/>
-                <strong>People</strong> and <strong>environment</strong> are at the heart of my concerns, therefore building <strong>responsible web solutions</strong> is a key value in my work.
-                </p>
-            </div> */}
-            {/* <div className="container__about-textLeft">
-                <p>
-                    Wether you want to bring build your <strong>project,</strong>start a <strong>business</strong> or talk about a <strong>job opportunity</strong>, feel free to reach out!
-                </p>
-            </div> */}
-            
-            {/* <div className="container__about-textRight">
+            </div>
+            <div className="container__about-textRight">
                 <p>
                     Today, I run <strong>BLOB Agency</strong>, a <strong>Webdesign</strong> company with incredible talents such as <a href="https://www.linkedin.com/in/alexiane-grieu-864084152/"><em>Alexiane Grieu</em></a>, UI/UX Designer.
                 </p>
-            </div> */}
+            </div>
+            <div className="container__about-textLeft">
+                <p>
+                    Wether you want to bring build your <strong>project,</strong>start a <strong>business</strong> or talk about a <strong>job opportunity</strong>, feel free to reach out!
+                </p>
+            </div>
+            
+           
             
         </section>
     )
--- src/Components/Portfolio/index.jsx
+++ src/Components/Portfolio/index.jsx
@@ -3,25 +3,7 @@ import { useEffect } from "react";
 import PortfolioCard from "../PortfolioCard";
 
 const Portfolio = () => {
-  
-     
 
-    // const scroller = (event) => {
-    //     let containerPortfolio = document.querySelector('.container__portfolio-card');
-    //     let arrow = event.target;
-    //     let cards = document.querySelectorAll('article');
-    
-
-    //     if(arrow.classList.contains("chevron-left")){
-           
-    //         containerPortfolio.scrollBy(-5, 0)
-    //     } else{
-    //         containerPortfolio.scrollTo(60, 200)
-    //     }
-        
-
-        
-    // }
     
 
     const [dataCard, setDataCard] = useState(undefined);
@@ -37,15 +19,12 @@ dataCard && (
                 PORTFOLIO
             </h2>
             <div className="container__portfolio-card">
-                <img src="/assets/chevron-left-solid.svg" alt="arrow-left" className="chevron chevron-left" />
             {dataCard && (
                 dataCard.map((data) => {
                     return(<PortfolioCard data = {data} key={data.id}/>)
 
                 })
-                
             )}
-                <img src="/assets/chevron-right-solid.svg" alt="arrow-left" className="chevron chevron-right"  />
             </div>
             
         
