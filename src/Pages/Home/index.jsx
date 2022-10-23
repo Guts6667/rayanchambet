a2a5a2cecef4096ed6ce87a527532ef792730e10
Rayan <rayanchambet1@gmail.com>
2022-10-23 16:56:58 +0200

    V.0.9.1

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -13,24 +13,10 @@ const Home = () => {
                     FOR THOSE WHO SEEK <strong className="strong-gradient">CHALLENGES</strong> AND CREATE <strong className="strong-gradient">SOLUTIONS.</strong>
                 </h2>
             <span className="container__home-divider"></span>
-            <div className="container-animation">
-            <div className="box-animation text-loop">
-            <p>INSPIRED WEB DEVELOPER </p>
-            <p>LEAN RESPONSIBLE CREATIONS</p>
-            <p>WORK WITH HEART AND MIND</p>
-            <p>PATHFINDER</p>
-            <p>SOFTWARE ARCHITECT</p>
-            <p>PROBLEM SOLVER</p>
-            </div>
-            <div className="box-animation2 text-loop2">
-            <p >INSPIRED WEB DEVELOPER </p>
-            <p>LEAN RESPONSIBLE CREATIONS</p>
-            <p>WORK WITH HEART AND MIND</p>
-            <p>PATHFINDER</p>
-            <p>SOFTWARE ARCHITECT</p>
-            <p>PROBLEM SOLVER</p>
-            </div>
-            </div>
+                <div className="container__home-subtitle">
+                    <p>INSPIRED WEB DEVELOPER LEAN RESPONSIBLE CREATIONS WORK WITH HEART AND MIND <br/>PATHFINDER SOFTWARE ARCHITECT PROBLEM SOLVER</p>
+                    
+                </div>
             
             <button className="container__home-btn"><a href="#contact">Start a project</a></button>
         </section>
