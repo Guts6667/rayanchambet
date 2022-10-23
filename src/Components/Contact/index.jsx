38901231ce5d2cf02e01700c06d7d60095faa87d
Rayan <rayanchambet1@gmail.com>
2022-10-23 15:53:27 +0200

    V.0.9

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
             
             <h2>CONTACT</h2>
             <div className="container__contact-form">
-            <p>Drop me a message to contact me, I'll get back to you as soon as possible!<br/> Whether you need to ask informations, discuss your project or talk about a job opportunity: <br/> I'll take the time hear you! <br/>See you soon 👋</p>
+            <p>Drop me a message to contact me, I'll get back to you as soon as possible!<br/> Whether you need to ask informations, discuss your project or talk about a job opportunity: <br/> I'll take the time to hear you! <br/>See you soon 👋</p>
             <FormContact />
             </div>
             
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -13,8 +13,26 @@ const Home = () => {
                     FOR THOSE WHO SEEK <strong className="strong-gradient">CHALLENGES</strong> AND CREATE <strong className="strong-gradient">SOLUTIONS.</strong>
                 </h2>
             <span className="container__home-divider"></span>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est praesentium illum facere voluptatem tempora ut incidunt quos a ad dolores doloribus harum vitae nesciunt possimus, dolorem minus consectetur eos fugit.</p>
-            <button className="container__home-btn">Start a project</button>
+            <div className="container-animation">
+            <div className="box-animation text-loop">
+            <p>INSPIRED WEB DEVELOPER </p>
+            <p>LEAN RESPONSIBLE CREATIONS</p>
+            <p>WORK WITH HEART AND MIND</p>
+            <p>PATHFINDER</p>
+            <p>SOFTWARE ARCHITECT</p>
+            <p>PROBLEM SOLVER</p>
+            </div>
+            <div className="box-animation2 text-loop2">
+            <p >INSPIRED WEB DEVELOPER </p>
+            <p>LEAN RESPONSIBLE CREATIONS</p>
+            <p>WORK WITH HEART AND MIND</p>
+            <p>PATHFINDER</p>
+            <p>SOFTWARE ARCHITECT</p>
+            <p>PROBLEM SOLVER</p>
+            </div>
+            </div>
+            
+            <button className="container__home-btn"><a href="#contact">Start a project</a></button>
         </section>
         <Portfolio />
         <About />
